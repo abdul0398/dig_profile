dd1e0a9efba64b819cfedefbb55e64ac4ea6af6e
abdul0398 <abdul77789@gmail.com>
2024-01-11 12:34:45 +0530

    some fixes

--- routes/profileRoute.js
+++ routes/profileRoute.js
@@ -156,7 +156,7 @@ router.get("/", verify, async(req,res)=>{
         res.redirect("/error");
     }
 }).post('/updateLinks', verify, async (req, res) => {
-    const { profile_id, data, templateSelected, phone, appointment_link, fb_link, insta_link, linkedin_link} = req.body;
+    const { profile_id, data, templateSelected, phone, fb_link, insta_link, linkedin_link} = req.body;
     console.log(req.body);
     const connection = await __pool.getConnection();
         try {
@@ -168,7 +168,7 @@ router.get("/", verify, async(req,res)=>{
             await connection.query(deleteQuery, [profile_id]);
             
             // Changing the template 
-            await connection.query('UPDATE profiles SET personal_link = ?, template_selected = ?, phone = ?, fb_link = ?, insta_link = ?, linkedin_link = ? WHERE id = ?', [appointment_link ==''?null:appointment_link, templateSelected, phone == ""?null:phone, fb_link ==""? null:fb_link, insta_link == ""?null:insta_link, linkedin_link == ""?null:linkedin_link, profile_id]);
+            await connection.query('UPDATE profiles SET template_selected = ?, phone = ?, fb_link = ?, insta_link = ?, linkedin_link = ? WHERE id = ?', [templateSelected, phone == ""?null:phone, fb_link ==""? null:fb_link, insta_link == ""?null:insta_link, linkedin_link == ""?null:linkedin_link, profile_id]);
             // Insert new link data
             for (const item of data) {
                 const query = 'INSERT INTO links (profilesId, type, link, name, heading, sort_order) VALUES (?, ?, ?, ?, ?, ?)';
