39f2c72e25560268f0049aabb1cfdd9f8f4cd6e8
abdul0398 <abdul77789@gmail.com>
2024-02-15 13:34:37 +0530

    fixes

--- routes/orders.js
+++ routes/orders.js
@@ -17,16 +17,8 @@ router.get("/orders", verify, isAdmin, async (req, res, next) => {
 .post("/api/orders/create", upload.array(), async (req, res) => {
     try {
         const {rawRequest} = req.body;
-        console.log(rawRequest);
-        console.log(JSON.parse(rawRequest));
-        const {
-            q2_fullName_1:name,
-            q3_mail_2 :email,
-            q4_phone_3:phone,
-            q20_typeA : cea_number,
-            q13_whatProperty:propertiesPortal,
-         } = JSON.parse(rawRequest);
-         console.log(name, email, phone, cea_number, propertiesPortal);
+        const data = extractData(JSON.parse(rawRequest))
+        console.log(data);
         res.status(200).json("Order Saved Successfully");
     } catch (error) {
         console.error(error);
@@ -53,6 +45,38 @@ router.get("/orders", verify, isAdmin, async (req, res, next) => {
         res.status(500).json({ message: "Internal Server Error" });
     }
 })
+
+
+
+function extractData(data) {
+    // Extracting specific fields
+    const fullName = `${data.q2_fullName_1.first} ${data.q2_fullName_1.last}`;
+    const email = data.q3_mail_2;
+    const phone = data.q4_phone_3.full;
+    const ceaNo = data.q20_typeA;
+    const propertiesListings = data.q13_whatProperty;
+    const propertyLinks = q7_longText_6;
+    const socials = data.q16_whatAre;
+    const socialLinks = data.q18_kindlyShare;
+    const aboutUs = data.q8_longText_7;
+    const feedback = data.q11_longText_10;
+
+  
+    // Extracting image links
+    const imageLinks = [
+      ...data.uploadYour,
+      ...data.addOn
+    ];
+  
+    // Returning the extracted data
+    return {
+      fullName,
+      email,
+      phone,
+      typeA,
+      imageLinks
+    };
+  }
 
 
 
