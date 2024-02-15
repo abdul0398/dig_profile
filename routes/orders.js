108db61f396ef438693cd00541ac2e81144402fc
abdul0398 <abdul77789@gmail.com>
2024-02-15 12:11:35 +0530

    fixes

--- routes/orders.js
+++ routes/orders.js
@@ -1,17 +1,87 @@
+const zipper = require("zip-local");
+var admz = require("adm-zip");
+const multer = require('multer');
 const router = require("express").Router();
+const path = require('path');
+const fs = require('fs/promises');
+const { verify, isAdmin, setUploadPath } = require("../middlewares/verify");
 
 
-router.post("/api/orders/create", async (req,res)=>{
-    if (req.headers.authorization) {
-        const token = req.headers.authorization.split(" ")[1];
-        if(token !== process.env.TOKEN){
-            return res.status(401).json({message:"Token is not valid or not present "});
-        }
-        console.log(req.body);
-        console.log("Token:", token);
-       res.status(200).json("Order Saved Successfully");
-    } else {
-        res.status(401).json({message:"Token is not valid or not present "});
+router.get("/orders", verify, isAdmin, async (req, res, next) => {
+    try {
+        const [orders] = await __pool.query("SELECT * FROM orders WHERE status = 'pending'");
+        res.render("orders.ejs", { orders });
+    } catch (error) {
+        console.log(error);
+        next();
     }
-});
+})
+.post("/api/orders/create", setUploadPath, async (req, res) => {
+    try {
+        req.upload.array('images')(req, res, async function (err) {
+            if (err instanceof multer.MulterError) {
+                return res.status(500).json(err);
+            } else if (err) {
+                return res.status(500).json(err);
+            }
+            console.log(req.files);
+            console.log(req.body);
+            // const directory = typeof req.file == Array ? req.files[0].destination : null;
+            // const { name, email, phone, other } = req.body;
+            // const insertQuery = "INSERT INTO orders (name, email, phone, other, path) VALUES (?, ?, ?, ?, ?)";
+            // await __pool.query(insertQuery, [name, email, phone, JSON.stringify(other), directory]);
+            // res.status(200).json("Order Saved Successfully");
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+})
+.get("/api/orders/status/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const [path] = await __pool.query("SELECT path FROM orders WHERE id = ?", [id]);
+        await fs.rmdir(path, { recursive: true });
+        const [order] = await __pool.query("UPDATE orders SET status = ? WHERE id = ? ", ["closed", id]);
+        res.status(200).json(order);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+})
+.get("/api/orders/delete/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        const [path] = await __pool.query("SELECT path FROM orders WHERE id = ?", [id]);
+        await fs.rmdir(path, { recursive: true });
+        const [order] = await __pool.query("DELETE FROM orders WHERE id = ?", [id]);
+        res.status(200).json(order);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}).get("/api/orders/download", verify, isAdmin, async (req, res) => {
+    const { id } = req.query;
+    try {
+        var to_zip = await fs.readdir(id);
+        var zip = new admz();
+        
+        to_zip.forEach(function (path) {
+            zip.addLocalFile(id + "/" + path);
+        });
+
+        const file_after_download = 'assets.zip';
+        const data = zip.toBuffer();
+        res.set('Content-Type', 'application/octet-stream');
+        res.set('Content-Disposition', `attachment; filename=${file_after_download}`);
+        res.set('Content-Length', data.length);
+        res.send(data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+})
+
+
+
 module.exports = router;
